0e370c3d24910fa569be24ce797e90284e862860
Y0sefTamer <programmeryosef@gmail.com>
2025-09-19 18:39:38 +0300

    prepare the care_link_backend canister

--- src/care_link_backend/src/lib.rs
+++ src/care_link_backend/src/lib.rs
@@ -1,4 +1,10 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
-}
+use candid::{CandidType, Decode, Deserialize, Encode};
+use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
+use ic_stable_structures::{BoundedStorable, DefaultMemoryImpl, StableBTreeMap, Storable};
+use std::{borrow::Cow, cell::RefCell};
+use candid::Principal;
+use ic_stable_structures::StableCell;
+
+type Memory = VirtualMemory<DefaultMemoryImpl>;
+const MAX_VALUE_SIZE: u32 = 5000;
+
